c5a2a3c5fff7b5b7d8c84843a8da5c396ed60486
haart <->
2026-08-23 22:53:51 +0300

    ability to add a obligatory executing of runable script file has been added (for adding local dependencis for each libraries or doing somthing else)

---
+++ cmake/for_libraries/add_prepare_script_to_library.cmake
@@ -0,0 +1,10 @@
+# ==========================================================
+# RUN EXECUTABLE SCIPT EACH TIME BEFORE BUILD LIBRARY
+# ==========================================================
+
+add_custom_target(${LIBRARY_NAME}_prepare
+  COMMAND ${CMAKE_COMMAND} -E env bash
+          ${CMAKE_CURRENT_SOURCE_DIR}/${PREPARE_SCRIPT_FILE}
+  WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+)
+add_dependencies(${LIBRARY_NAME} ${LIBRARY_NAME}_prepare)
